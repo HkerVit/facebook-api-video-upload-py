2a83d55fded25a8b72693a819679d8e93b3a49ef
root <root@vultr.guest>
2021-03-10 03:52:50 +0000

    add

--- youtube-upload/uploadVideo.js
+++ youtube-upload/uploadVideo.js
@@ -4,9 +4,9 @@
             const args = {
               token: "EAADZBROPXRHgBAG80dprQ3QVdROZAycaZCRBlQh7ZCk0HZBxhGvqwflEANMlTb20fK8cB6jBnx90ZAXElpaiMd6bjAPsZBAHkXQvKw9zwHdnFs7NHIdXvelbW6PTuZBqoO58B4m5kdJuAYg5JxLGAXxVqZCA9OJmXHlTC68QLSl41bQZDZD",
               id: "182766363336548",
-              stream: fs.createReadStream('../douyin-downloader/Download/虫哥说电影/魔性解读中二爆笑校园神剧《终极一班》，中二魂爆表了 #终极一班.mp4'),
-              title:"魔性解读中二爆笑校园神剧《终极一班》，中二魂爆表了 #终极一班" ,
-              description:"魔性解读中二爆笑校园神剧《终极一班》，中二魂爆表了 #终极一班",
+              stream: fs.createReadStream('../douyin-downloader/Download/虫哥说电影/魔性解读土嗨蹦迪神剧《封神英雄榜》 这部剧是太雷人#封神英雄榜.mp4'),
+              title:"魔性解读土嗨蹦迪神剧《封神英雄榜》 这部剧是太雷人#封神英雄榜" ,
+              description:"魔性解读土嗨蹦迪神剧《封神英雄榜》 这部剧是太雷人#封神英雄榜",
             };
             fbUpload(args).then((res) => {
               console.log('res: ', res);
